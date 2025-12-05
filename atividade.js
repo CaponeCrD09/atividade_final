303fd19e4a0a8a46677b9d310d14931224b5e4ef
anthonydutra <anthonydutra189@gmail.com>
2025-12-05 20:59:47 -0300

    mudanças mudei o codigo do arquivo buscar Produtos.js para a atividade.js

---
+++ atividade.js
@@ -0,0 +1,10 @@
+//versão previa/ primeira ideia
+
+
+if(id == array){
+
+
+    console.log("")
+}else{
+    console.log("Produto não fora de estoque ou inexistente")
+};  
--- buscarProdutos.js
+++ buscarProdutos.js
@@ -1,12 +1,3 @@
 //versão previa/ primeira ideia
 
 
-
-    if(id == array){
-
-
-
-        console.log("")
-    }else{
-        console.log("Produto não fora de estoque ou inexistente")
-    };  
